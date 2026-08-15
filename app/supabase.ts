274b8459ccbf1ca4ba279991d508c913fe42bd48
EstudioWeremko <estudioweremko@gmail.com>
2026-08-15 16:58:22 -0300

    Actualizar clave pública de Supabase

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://aioxkxhfxlilynygripl.supabase.co";
-const supabaseAnonKey = "sb_publishable_Fg6trPrZcm_EB5dmYpyJT0_zPGzB71T";
+const supabaseAnonKey = "sb_publishable_Fg6trPrZcm_EB5dmYpyJTQ_zPGzB71T";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
