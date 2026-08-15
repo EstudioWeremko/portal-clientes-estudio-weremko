90bda0a9f3611f81d6df0ae2517a9057701bb298
EstudioWeremko <estudioweremko@gmail.com>
2026-08-15 16:43:49 -0300

    Corregir URL de conexión a Supabase

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://aioxkxhfylilynygriрl.supabase.co";
+const supabaseUrl = "https://aioxkxhfxlilynygripl.supabase.co";
 const supabaseAnonKey = "sb_publishable_Fg6trPrZcm_EB5dmYpyJT0_zPGzB71T";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
